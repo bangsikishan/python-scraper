e87f1144837f845a70db2a60460a637c582b462c
KM <bangsikishan@duck.com>
2023-11-29 17:24:05 +0545

    Add functions to get download links

--- scripts/procurement.py
+++ scripts/procurement.py
@@ -4,6 +4,8 @@ import emoji
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from functions import initalize_webdriver
 from config import Values
@@ -43,19 +45,46 @@ def get_procurement_bids(url: str):
 
     # ACCESS & FILTER ELEMENTS
     bids = {}
-    bid_elements = driver.find_elements(By.CLASS_NAME, "rt-tr-group")[:number_of_bids]
-    for index, bid_element in enumerate(bid_elements):
+    for index in range(number_of_bids):
+        bid_element = driver.find_element(By.XPATH, f"/html/body/div/div/div/div/main/div/div[2]/div/div/div[3]/div[1]/div[2]/div[{index+1}]")
         bid_no = bid_element.find_element(By.XPATH, ".//div/div[2]").text
         title = bid_element.find_element(By.XPATH, ".//div/div[1]").text
         due_date = bid_element.find_element(By.XPATH, ".//div/div[5]").text
         
-        bids[index] = {
-            "ecgain": Values.ECGAIN.value,
-            "base_url": Values.URL.value,
-            "bid_no": bid_no,
-            "title": title,
-            "due_date": due_date
-        }
+        bid_element.click()
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div/main/div[5]/div/ul/li[3]/a"))
+        ).click()
+        time.sleep(2)
+
+        driver.find_element(By.XPATH, "/html/body/div/div/div/div/main/div[5]/div/ul/li[3]/a").click()
+        time.sleep(1)
+
+        driver.find_element(By.XPATH, "/html/body/div/div/div/div/main/div[5]/div/div/div/div/div[2]/div[1]/button").click()
+        time.sleep(1)
+
+        driver.find_element(By.CLASS_NAME, "fa-download").click()
+        time.sleep(2)
+
+        driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div/div[2]/div/button[1]").click()
+        time.sleep(1)
+
+        download_link = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div/div[2]/div/a"))
+        ).get_attribute("href")
+
+        print(download_link)
+        
+        driver.get(url)
+        time.sleep(5)
+
+        # bids[index] = {
+        #     "ecgain": Values.ECGAIN.value,
+        #     "base_url": Values.URL.value,
+        #     "bid_no": bid_no,
+        #     "title": title,
+        #     "due_date": due_date
+        # }
 
     # CLOSE THE DRIVER
     driver.quit()
