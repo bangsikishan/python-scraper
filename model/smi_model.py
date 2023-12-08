5f0ee464902e4dbf5a4d68eaeec2019685039391
KM <bangsikishan@duck.com>
2023-12-08 16:59:27 +0545

    Updated model

--- model/smi_model.py
+++ model/smi_model.py
@@ -10,12 +10,12 @@ class SMI(Base):
     __tablename__ = "smis"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    hash: Mapped[str] = mapped_column(String(64), nullable=False)
+    hash: Mapped[str] = mapped_column(String(32), nullable=False)
     ecgains: Mapped[str] = mapped_column(String(21), nullable=False)
     bidno: Mapped[str] = mapped_column(String(20), nullable=False)
     title: Mapped[str] = mapped_column(String(40), nullable=False)
-    duedate: Mapped[str] = mapped_column(String(10), nullable=False)
-    baseurl: Mapped[str] = mapped_column(String(150), nullable=False)
-    fileurl: Mapped[str] = mapped_column(String(150), nullable=False)
+    duedate: Mapped[str] = mapped_column(String(13), nullable=False)
+    baseurl: Mapped[str] = mapped_column(String(255), nullable=False)
+    fileurl: Mapped[str] = mapped_column(String(255), nullable=False)
     filename: Mapped[str] = mapped_column(String(150), nullable=False)
     filesize: Mapped[str] = mapped_column(String(10), nullable=False)
